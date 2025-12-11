33cb86328438a63bf9a01d098a72de7a9a56cc3c
Dan Wright <dan@dwright.org>
2025-12-11 17:35:09 -0500

    Safe Request Mode: fix YouTube detection and add async header handler logging

--- js/safe-request/safe-request-handler.js
+++ js/safe-request/safe-request-handler.js
@@ -9,34 +9,56 @@ console.debug('[CSL] Safe Request Mode handler loaded');
 
 /**
  * Handle onBeforeSendHeaders - add global headers and YouTube-Restrict
+ * NOTE: Must be async because we need to load state from storage
  */
-async function handleBeforeSendHeaders(details) {
-  try {
-    const state = await loadState();
-    
-    const shouldApply = await shouldApplySafeRequest(state);
-    
-    if (!shouldApply) {
-      return;
+function handleBeforeSendHeaders(details) {
+  // Return a Promise that Firefox MV3 can handle
+  return (async () => {
+    try {
+      console.debug('[CSL] onBeforeSendHeaders called for:', details.url);
+      const state = await loadState();
+      
+      const shouldApply = await shouldApplySafeRequest(state);
+      console.debug('[CSL] shouldApply:', shouldApply);
+      
+      if (!shouldApply) {
+        console.debug('[CSL] Skipping - Safe Request Mode not active');
+        return {};
+      }
+      
+      const isProcessable = shouldProcessUrl(details.url);
+      console.debug('[CSL] isProcessable:', isProcessable);
+      
+      if (!isProcessable) {
+        console.debug('[CSL] Skipping - URL not processable');
+        return {};
+      }
+      
+      const config = state.safeRequestMode;
+      let headers = details.requestHeaders || [];
+      
+      console.debug('[CSL] onBeforeSendHeaders - original headers:', headers.length);
+      
+      // Apply header enforcement
+      headers = enforceSafeHeaders(headers, details.url, config);
+      
+      console.debug('[CSL] onBeforeSendHeaders - modified headers:', headers.length);
+      
+      // Log the actual headers being returned
+      const preferHeader = headers.find(h => h.name.toLowerCase() === 'prefer');
+      const youtubeHeader = headers.find(h => h.name.toLowerCase() === 'youtube-restrict');
+      console.debug('[CSL] Prefer header:', preferHeader);
+      console.debug('[CSL] YouTube-Restrict header:', youtubeHeader);
+      
+      const result = { requestHeaders: headers };
+      console.debug('[CSL] onBeforeSendHeaders - returning:', JSON.stringify(result, null, 2));
+      
+      return result;
+    } catch (err) {
+      console.error('[CSL] Error in onBeforeSendHeaders:', err);
+      return {};
     }
-    
-    const isProcessable = shouldProcessUrl(details.url);
-    
-    if (!isProcessable) {
-      return;
-    }
-    
-    const config = state.safeRequestMode;
-    let headers = details.requestHeaders || [];
-    
-    // Apply header enforcement
-    headers = enforceSafeHeaders(headers, details.url, config);
-    
-    return { requestHeaders: headers };
-  } catch (err) {
-    console.error('[CSL] Error in onBeforeSendHeaders:', err);
-    return;
-  }
+  })();
 }
 
 /**
